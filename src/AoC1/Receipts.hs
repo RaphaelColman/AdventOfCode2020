f77905c9f34e0a3b03abb0c85f6cd3765ba88f98
Raphael.Colman <rcolman@ybs.com>
2020-12-01 13:53:15 +0000

    Day 1 part 2

--- src/AoC1/Receipts.hs
+++ src/AoC1/Receipts.hs
@@ -8,7 +8,8 @@ main = do
   handle <- openFile "src/AoC1/input.txt" ReadMode  
   contents <- hGetContents handle
   let expenses = parseContents contents
-  let result = uncurry (*) $ goodPair $ pairs expenses
+  --let result = uncurry (*) $ goodPair $ pairs expenses
+  let result = multiplyTriplet $ goodTriplet $ triplets expenses
   print result
 
 parseContents :: String -> [Int]
@@ -17,5 +18,14 @@ parseContents = map read . lines
 pairs :: [a] -> [(a, a)]
 pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
 
+triplets :: [a] -> [(a, a, a)]
+triplets l = [(x,y,z) | (x:ys) <- tails l, (y:zs) <- tails ys, z <- zs]
+
 goodPair :: [(Int, Int)] -> (Int, Int)
 goodPair = head . filter (\p -> uncurry (+) p == 2020)
+
+goodTriplet :: [(Int, Int, Int)] -> (Int, Int, Int)
+goodTriplet = head . filter (\(a, b, c) -> a + b + c == 2020)
+
+multiplyTriplet :: (Num a) => (a, a, a) -> a
+multiplyTriplet (x, y ,z) = x * y * z
