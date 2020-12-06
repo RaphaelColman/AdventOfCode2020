f0d6411c397a06443d591614e674b9993f93b1b0
Raphael Colman <raphael.colman@infinityworks.com>
2020-12-06 11:16:25 +0000

    Refactor

--- src/AoC6/Customs.hs
+++ src/AoC6/Customs.hs
@@ -1,15 +1,15 @@
 module AoC6.Customs where
 
-import Common.Utils
-import Data.List.Split
-import qualified Data.Set as S
+import           Common.Utils
+import           Data.List.Split
+import qualified Data.Set        as S
 
 aoc6 :: IO ()
 aoc6 = do
   contents <- getInputFile 6
-  print $ splitToGroups contents
-  print $ sumUnique $ splitToGroups contents
-  print $ sumIntersections $ splitToGroups contents
+  let groups = splitToGroups contents
+  print $ sumUnique groups
+  print $ sumIntersections groups
 
 splitToGroups :: String -> [[String]]
 splitToGroups = map lines . splitOn "\n\n"
@@ -18,13 +18,7 @@ sumUnique :: [[String]] -> Int
 sumUnique = sum . map (length . S.fromList. concat)
 
 sumIntersections :: [[String]] -> Int
-sumIntersections ll = sum $ map forSingleGroup ll
-
-intersectionFoldableSets :: (Ord a, Eq a) => [S.Set a] -> S.Set a
-intersectionFoldableSets sets = foldl S.intersection (head sets) sets
-
-testGroups :: [[String]]
-testGroups = [["abc"],["a","b","c"],["ab","ac"],["a","a","a","a"],["b"]]
-
-forSingleGroup :: Ord a => [[a]] -> Int
-forSingleGroup xs = length $ intersectionFoldableSets $ map S.fromList xs
+sumIntersections = sum . map countIntersections
+    where countIntersections = length
+                                . foldl1 S.intersection
+                                . map S.fromList
