909c3c7af736d951d51da469d74fb41e80f20448
Raphael Colman <raphael.colman@infinityworks.com>
2020-12-06 11:05:48 +0000

    Day 6 part 2

--- src/AoC6/Customs.hs
+++ src/AoC6/Customs.hs
@@ -7,10 +7,24 @@ import qualified Data.Set as S
 aoc6 :: IO ()
 aoc6 = do
   contents <- getInputFile 6
+  print $ splitToGroups contents
   print $ sumUnique $ splitToGroups contents
+  print $ sumIntersections $ splitToGroups contents
 
 splitToGroups :: String -> [[String]]
 splitToGroups = map lines . splitOn "\n\n"
 
 sumUnique :: [[String]] -> Int
-sumUnique = sum . map (length . S.fromList. concat )
+sumUnique = sum . map (length . S.fromList. concat)
+
+sumIntersections :: [[String]] -> Int
+sumIntersections ll = sum $ map forSingleGroup ll
+
+intersectionFoldableSets :: (Ord a, Eq a) => [S.Set a] -> S.Set a
+intersectionFoldableSets sets = foldl S.intersection (head sets) sets
+
+testGroups :: [[String]]
+testGroups = [["abc"],["a","b","c"],["ab","ac"],["a","a","a","a"],["b"]]
+
+forSingleGroup :: Ord a => [[a]] -> Int
+forSingleGroup xs = length $ intersectionFoldableSets $ map S.fromList xs
