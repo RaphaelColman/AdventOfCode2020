2e11b45c9ccea9e3f69788a572418b553e37409a
Raphael.Colman <rcolman@ybs.com>
2020-12-08 09:00:28 +0000

    Day 8 part 2

--- src/AoC8/Handheld.hs
+++ src/AoC8/Handheld.hs
@@ -10,6 +10,8 @@ aoc8 = do
   let memory = initMemory $ parseInstructions contents
   let result = runMemory memory
   print $ accumulator <$> result
+  let part2Result = findSuccessfulFlip memory
+  print $ accumulator <$> part2Result
 
 type Instruction = (String, Int)
 
@@ -31,7 +33,17 @@ initMemory instr = Mem 0 instr 0 S.empty
 runMemory :: Memory -> Maybe Memory
 runMemory mem@(Mem position' _ _ visited)
     | S.member position' visited = Just mem
+    | terminatedSuccessfully mem = Just mem
     | otherwise = step mem >>= runMemory
+
+findSuccessfulFlip :: Memory -> Maybe Memory
+findSuccessfulFlip mem = go 0 mem
+    where go indexToFlip mem' = do
+            flipped <- flipInstruction mem' indexToFlip
+            result <- runMemory flipped
+            if terminatedSuccessfully result
+                then pure result
+                else go (indexToFlip + 1) mem
 
 step :: Memory -> Maybe Memory
 step (Mem position' instructions' accumulator visited) = do
@@ -41,3 +53,15 @@ step (Mem position' instructions' accumulator visited) = do
         "jmp" -> Just $ Mem (position' + number) instructions' accumulator newVisited
         "nop" -> Just $ Mem (position' + 1) instructions' accumulator newVisited
         "acc" -> Just $ Mem (position' + 1) instructions' (accumulator + number) newVisited
+
+terminatedSuccessfully :: Memory -> Bool
+terminatedSuccessfully (Mem position' instructions' _ _) = position' >= length instructions'
+
+flipInstruction :: Memory -> Int -> Maybe Memory
+flipInstruction (Mem position' instructions' accumulator visited) index = do
+    (instr, amount) <- Seq.lookup index instructions'
+    let newInstructions = case instr of 
+            "jmp" -> Seq.update index ("nop", amount) instructions'
+            "nop" -> Seq.update index ("jmp", amount) instructions'
+            _ -> instructions'
+    pure $ Mem position' newInstructions accumulator visited
