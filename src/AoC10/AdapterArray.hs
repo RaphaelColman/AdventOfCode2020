48f235f3d117fde7f4c1913793e0804a05286005
Raphael Colman <raphael.colman@infinityworks.com>
2020-12-10 19:44:17 +0000

    Day 10 part 2

--- src/AoC10/AdapterArray.hs
+++ src/AoC10/AdapterArray.hs
@@ -1,10 +1,10 @@
 module AoC10.AdapterArray where
 
-import Common.Utils
-import Data.Sort
-import qualified Data.Map as M
-import Data.List
-import Data.List.Split
+import           Common.Utils
+import           Data.List
+import           Data.List.Split
+import qualified Data.Map        as M
+import           Data.Sort
 
 aoc10 :: IO ()
 aoc10 = do
@@ -12,6 +12,7 @@ aoc10 = do
   let adapters = parseContents contents
   let sortedAdapters = sort adapters
   print $ freqs $ joltDistribution sortedAdapters
+  print $ combinations sortedAdapters
 
 parseContents :: String -> [Int]
 parseContents = map read . lines
@@ -21,14 +22,20 @@ differenceDistribution xs = zipWith (-) (tail xs) xs
 
 --add the first and last differences to the beginning of the list
 joltDistribution :: [Int] -> [Int]
-joltDistribution xs = 3 : head xs : differenceDistribution xs
+joltDistribution xs = (head xs : differenceDistribution xs) ++ [3]
 
 
-sumToWith3s2s1s:: Int -> Int
-sumToWith3s2s1s i = undefined
+sumToWith3s2s1s :: Int -> [[Int]]
+sumToWith3s2s1s = go
+  where go :: Int-> [[Int]]
+        go target
+            | target < 0 = []
+            | target == 0 = [[]]
+            | otherwise = map (1 :) (go (target - 1))
+            ++ map (2 :) (go (target - 2))
+            ++ map (3 :) (go (target - 3))
 
-groupN :: Int -> [a] -> [[a]]
-groupN _ [] = []
-groupN n l
-  | n > 0 = take n l : groupN n (drop n l)
-  | otherwise = error "Negative or zero n"
+combinations :: [Int] -> Int
+combinations xs = product $ map (length . sumToWith3s2s1s) groupsOfOneLengths
+  where distribution = joltDistribution xs
+        groupsOfOneLengths = map length $ filter (all (== 1)) $ group distribution
