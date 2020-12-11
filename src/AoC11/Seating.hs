605c85513c2eeaa397056fdee5e5cccf90e236b5
Raphael.Colman <rcolman@ybs.com>
2020-12-11 15:43:34 +0000

    Day 11 refactor

--- src/AoC11/Seating.hs
+++ src/AoC11/Seating.hs
@@ -1,94 +1,56 @@
 module AoC11.Seating where
 
 import Common.Utils
-import Control.Lens
-import Data.List
 import qualified Data.Map as M
-import Data.Maybe
 import Linear.V2
 import Linear.Vector
-import System.IO
 
 aoc11 :: IO ()
 aoc11 = do
   contents <- getInputFile 11
   let seatingArea = parseToMap contents
-  --print $ runUntilStable seatingArea
-  print $ runUntilStableNew seatingArea
-  print "done"
+  print $ runUntilStable (RP numberOfOccupiedAdjacentSeats' 4)  seatingArea
+  print $ runUntilStable (RP numberOfVisibleOccupiedSeats 5)  seatingArea
 
 type SeatingArea = M.Map (V2 Int) Char
+
+data RunParameters = RP {
+  numberOfOccupiedSeatsFunction :: SeatingArea -> V2 Int -> Int,
+  threshold :: Int
+}
 
 parseToMap :: String -> SeatingArea
 parseToMap s = M.fromList $ map (\((x, y), c) -> (V2 x y, c)) $ enumerateMultilineString s
 
 allAdjacents :: V2 Int -> [V2 Int]
-allAdjacents v =
-  [ v + unit _x,
-    v - unit _x,
-    v + unit _y,
-    v - unit _y,
-    v + unit _x + unit _y,
-    v + unit _x - unit _y,
-    v - unit _x + unit _y,
-    v - unit _x - unit _y
-  ]
+allAdjacents v = map (v +) allDirections
 
-step :: SeatingArea -> SeatingArea
-step sa = M.mapWithKey foo sa
-  where
-    foo coord value = convertSeat value [M.findWithDefault '.' c sa | c <- allAdjacents coord]
+stepFun :: RunParameters -> SeatingArea -> SeatingArea
+stepFun (RP fun threshold) sa = M.mapWithKey (\coord value -> convertSeat threshold value (fun sa coord)) sa
 
-convertSeat :: Char -> String -> Char
-convertSeat currentValue adjacents
-  | currentValue == 'L' =
-    if all (\c -> c == 'L' || c == '.') adjacents
-      then '#'
-      else currentValue
-  | currentValue == '#' =
-    if length (filter (== '#') adjacents) >= 4
-      then 'L'
-      else currentValue
-  | otherwise = currentValue
-
-runUntilStable :: SeatingArea -> Int
-runUntilStable = go 0
-  where
-    go numSteps seatingArea
-      | nextSeatingArea == seatingArea = length $ M.filter (== '#') seatingArea
-      | otherwise = go (numSteps + 1) nextSeatingArea
-      where
-        nextSeatingArea = step seatingArea
-
-runInteractive :: SeatingArea -> IO ()
-runInteractive sa = do
-  putStr $ renderVectorMap sa
-  _ <- getLine
-  runInteractive (stepNew sa)
-
-runUntilStableNew :: SeatingArea -> Int
-runUntilStableNew = go 0
-  where
-    go numSteps seatingArea
-      | nextSeatingArea == seatingArea = length $ M.filter (== '#') seatingArea
-      | otherwise = go (numSteps + 1) nextSeatingArea
-      where
-        nextSeatingArea = stepNew seatingArea
-
-
-stepNew :: SeatingArea -> SeatingArea
-stepNew sa = M.mapWithKey foo sa
-    where foo coord value = convertSeatNew value $ numberOfVisibleOccupiedSeats sa coord
-
-convertSeatNew :: Char -> Int -> Char
-convertSeatNew current occupied
-    | current == 'L' = if occupied == 0 then '#' else current
-    | current == '#' = if occupied >= 5 then 'L' else current
-    | otherwise = current
 
 numberOfVisibleOccupiedSeats :: SeatingArea -> V2 Int -> Int
 numberOfVisibleOccupiedSeats sa coord = length $ filter (=='#') $ map (travelToSeatOrEdgeInDirection sa coord) allDirections
+
+numberOfOccupiedAdjacentSeats' :: SeatingArea -> V2 Int -> Int
+numberOfOccupiedAdjacentSeats' sa = length . filter (=='#') . map (flip (M.findWithDefault '=') sa) . allAdjacents
   
+
+convertSeat :: Int -> Char -> Int -> Char
+convertSeat threshold current occupied 
+    | current == 'L' = if occupied == 0 then '#' else current
+    | current == '#' = if occupied >= threshold then 'L' else current
+    | otherwise = current
+
+runUntilStable :: RunParameters -> SeatingArea -> Int
+runUntilStable rp = go 0
+  where
+    go numSteps seatingArea
+      | nextSeatingArea == seatingArea = length $ M.filter (== '#') seatingArea
+      | otherwise = go (numSteps + 1) nextSeatingArea
+      where
+        nextSeatingArea = stepFun rp seatingArea
+
 travelToSeatOrEdgeInDirection :: SeatingArea -> V2 Int -> V2 Int -> Char
 travelToSeatOrEdgeInDirection sa = go
   where go start direction
