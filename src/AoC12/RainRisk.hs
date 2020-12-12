23b54676143f72373ac5ca386b97974e20289bab
Raphael Colman <raphael.colman@infinityworks.com>
2020-12-12 20:25:21 +0000

    Day 12 part 2

--- src/AoC12/RainRisk.hs
+++ src/AoC12/RainRisk.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module AoC12.RainRisk where
 
-import Common.Utils
-import Linear.V2
-import Linear.Vector
-import Prelude hiding (Right, Left)
+import           Common.Utils
+import           Linear.V2
+import           Linear.Vector
+import           Prelude       hiding (Left, Right)
 
 aoc12 :: IO ()
 aoc12 = do
     contents <- getInputFile 12
     let instructions = parseContents contents
     print $ part1 instructions
+    print $ part2 instructions
 
 type Instruction = (Char, Int)
 
@@ -18,11 +19,17 @@ data Direction = North | East | South | West deriving (Eq, Show, Enum, Bounded)
 data Turn = Right | Left deriving (Eq, Show, Enum, Bounded)
 data Vessel = Ship {
     location :: V2 Int,
-    facing :: Direction
+    facing   :: Direction
+} | WaypointShip {
+    location :: V2 Int,
+    waypoint :: V2 Int
 } deriving (Show, Eq)
 
 part1 :: [Instruction] -> Int
-part1 = manhattanDistance . run initShip 
+part1 = manhattanDistance . run initShip
+
+part2 :: [Instruction] -> Int
+part2 = manhattanDistance . run initWaypointShip
 
 parseContents :: String -> [Instruction]
 parseContents = map toInstruction . lines
@@ -31,35 +38,60 @@ parseContents = map toInstruction . lines
 initShip :: Vessel
 initShip = Ship (V2 0 0) East
 
+initWaypointShip :: Vessel
+initWaypointShip = WaypointShip (V2 0 0) (V2 10 1)
+
 run :: Vessel -> [Instruction] -> Vessel
 run = foldl step
 
 step :: Vessel -> Instruction -> Vessel
-step ves@(Ship _ facing) (instr, amount) = case instr of
+step ves (instr, amount) = case instr of
     'N' -> moveCardinal ves North amount
     'E' -> moveCardinal ves East amount
     'S' -> moveCardinal ves South amount
     'W' -> moveCardinal ves West amount
-    'L' -> turnVessel ves Left amount
-    'R' -> turnVessel ves Right amount
-    'F' -> moveCardinal ves facing amount
-    _ -> undefined
+    'L' -> turn ves Left amount
+    'R' -> turn ves Right amount
+    'F' -> moveFoward ves amount
+    _   -> undefined
+
+moveFoward :: Vessel -> Int -> Vessel
+moveFoward vessel amount = case vessel of
+    (Ship _ facing) -> moveCardinal vessel facing amount
+    (WaypointShip location waypoint) -> WaypointShip (location + amount *^ waypoint) waypoint
+
+moveCardinal :: Vessel -> Direction -> Int -> Vessel
+moveCardinal ship direction amount = case ship of
+    (Ship location facing) -> Ship (location + amount *^ vector) facing
+    (WaypointShip location waypoint) -> WaypointShip location (waypoint + amount *^ vector)
+    where vector :: V2 Int = case direction of
+                North -> unit _y
+                East  -> unit _x
+                South -> - unit _y
+                West  -> - unit _x
+
+turn :: Vessel -> Turn -> Int -> Vessel
+turn vessel turn amount = case vessel of
+    (Ship _ _)         -> turnVessel vessel turn amount
+    (WaypointShip _ _) -> rotateWaypoint vessel turn amount
+
+rotateWaypoint :: Vessel -> Turn -> Int -> Vessel
+rotateWaypoint (WaypointShip location waypoint) direction amount = WaypointShip location newWaypoint
+    where turns = amount `div` 90
+          newWaypoint = last $ take turns $ tail $ iterate (rotateVector direction) waypoint
 
 turnVessel :: Vessel -> Turn -> Int -> Vessel
 turnVessel (Ship location facing) turn amount = Ship location newDirection
     where rotation = case turn of
             Right -> enumNext
-            Left -> enumPrev
+            Left  -> enumPrev
           turns = amount `div` 90
           newDirection = last $ take turns $ tail $ iterate rotation facing
 
-moveCardinal :: Vessel -> Direction -> Int -> Vessel
-moveCardinal (Ship location facing) direction amount = Ship (location + amount *^ vector) facing
-    where vector :: V2 Int = case direction of
-                North -> unit _y
-                East -> unit _x
-                South -> - unit _y
-                West -> - unit _x
+rotateVector :: Turn -> V2 Int -> V2 Int
+rotateVector turn (V2 x y) = case turn of
+    Right -> V2 y (-x)
+    Left  -> V2 (-y) x
 
 manhattanDistance :: Vessel -> Int
 manhattanDistance = sum . abs . location
--- test/AoC12/RainRiskSpec.hs
+++ test/AoC12/RainRiskSpec.hs
@@ -7,9 +7,12 @@ import Text.Format
 import System.Directory
 
 spec :: Spec
-spec = describe "Part 1 test" $
+spec = describe "E2E Test" $ do
         it "Should do part 1" $
             part1Result `shouldReturn` 757
+        
+        it "Should do part 2" $
+            part2Result `shouldReturn` 51249
 
 
 readInput :: IO String
@@ -23,3 +26,9 @@ part1Result = do
     contents <- readInput
     let instructions = parseContents contents
     pure $ part1 instructions
+
+part2Result :: IO Int
+part2Result = do
+    contents <- readInput
+    let instructions = parseContents contents
+    pure $ part2 instructions
