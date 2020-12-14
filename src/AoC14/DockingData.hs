8b531cae879c78beedef43119d5d473c8d26230b
Raphael Colman <raphael.colman@infinityworks.com>
2020-12-14 19:46:30 +0000

    Minor refactor

--- src/AoC14/DockingData.hs
+++ src/AoC14/DockingData.hs
@@ -1,37 +1,33 @@
 module AoC14.DockingData where
 
-import Common.Utils
-import Data.Bits
-import Data.Char
-import Data.Char (digitToInt)
-import qualified Data.IntMap as IM
-import qualified Data.IntSet as IS
-import Data.List
-import Data.List.Split
-import Data.Maybe (fromJust, listToMaybe)
-import Data.Monoid
-import Debug.Trace
-import Numeric (readInt)
+import           Common.Utils
+import           Data.Bits
+import           Data.Char
+import qualified Data.IntMap     as IM
+import qualified Data.IntSet     as IS
+import           Data.List
+import           Data.List.Split
+import           Data.Maybe      (fromJust, listToMaybe)
+import           Data.Monoid
+import           Numeric         (readInt)
 
 aoc14 :: IO ()
 aoc14 = do
   contents <- getInputFile 14
-  --print $ part1 contents
+  print $ part1 contents
   print $ part2 contents
-
-type Mask = [Int -> Int]
 
 type Memory = IM.IntMap Int
 
 data MemoryState = MS
-  { memory :: Memory,
+  { memory      :: Memory,
     currentMask :: String
   }
   deriving (Eq, Show)
 
 data WriteOperation = WO
   { location :: Int,
-    number :: Int
+    number   :: Int
   }
   deriving (Show, Eq)
 
@@ -52,8 +48,6 @@ part2 =
 initMemoryState :: MemoryState
 initMemoryState = MS IM.empty "" --This will break if the first line is not a mask
 
-parseMaskline :: String -> Mask
-parseMaskline str = let [_, _, maskString] = words str in readMaskString maskString
 
 parseWOLine :: String -> WriteOperation
 parseWOLine str = WO (toNumber memString) (toNumber numString)
@@ -61,28 +55,15 @@ parseWOLine str = WO (toNumber memString) (toNumber numString)
     [memString, numString] = splitOn " = " str
     toNumber = read . filter isDigit
 
-readMaskString :: String -> Mask
-readMaskString str = map parseCharacter enumerated
-  where
-    enumerated = zip (reverse str) [0 ..]
-    parseCharacter (ch, index)
-      | ch == 'X' = id
-      | ch == '1' = flip setBit index
-      | ch == '0' = flip clearBit index
-
-applyMask :: Mask -> Int -> Int
-applyMask mask int = foldl (\a m -> m a) int mask
-
 stepMemoryState :: MemoryState -> String -> MemoryState
 stepMemoryState memState@(MS memory' _) str
-  | take 4 str == "mask" = MS memory' str
+  | take 4 str == "mask" = MS memory' $ parseMaskToString str
   | take 3 str == "mem" = applyWO memState $ parseWOLine str
 
 applyWO :: MemoryState -> WriteOperation -> MemoryState
 applyWO (MS memory' currentMask') (WO location' number') = MS newMemory currentMask'
   where
-    mask = parseMaskline currentMask' --this will reread the mask every time.
-    numberToWrite = applyMask mask number'
+    numberToWrite = applyMask currentMask' number'
     newMemory = IM.insert location' numberToWrite memory'
 
 parseMaskToString :: String -> String
@@ -98,6 +79,15 @@ applyWOV2 (MS memory' currentMask') (WO location' number') = MS newMemory curren
   where
     locations = applyV2Mask currentMask' location'
     newMemory = foldl (\mem l -> IM.insert l number' mem) memory' locations
+
+applyMask :: String -> Int -> Int
+applyMask str int = foldl (\a m -> m a) int maskOperations
+  where maskOperations = map parseCharacter enumerated
+        enumerated = zip (reverse str) [0 ..]
+        parseCharacter (ch, index)
+          | ch == 'X' = id
+          | ch == '1' = flip setBit index
+          | ch == '0' = flip clearBit index
 
 applyV2Mask :: String -> Int -> [Int]
 applyV2Mask str int = applyFloatingBits floatingBits applyOnes
