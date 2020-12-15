7757e3562684a94476d668e3143dde1e3dd31712
Raphael.Colman <rcolman@ybs.com>
2020-12-15 08:52:28 +0000

    Day 15 WIP

--- src/AoC15/RambunctiousRecitation.hs
+++ src/AoC15/RambunctiousRecitation.hs
@@ -4,17 +4,18 @@ import qualified Data.IntMap as IM
 
 aoc15 :: IO ()
 aoc15 = do
-    print $ recite2020 input 2020
+    print $ recite input 2020
     print $ recite2020 input 30000000
-    print "hello"
 
 input :: [Int]
 input = [18,8,0,5,4,1,20]
 
 recite2020 :: [Int] -> Int -> Int
-recite2020 ints goal = lastEntered $ reciteRecursive $ PS toIntMap (length ints + 1) (last ints) (reverse ints) goal
-    where toIntMap = IM.fromList $ zip ints [[x] | x <- [1..]]
+recite2020 ints goal = lastEntered $ recite ints goal
 
+recite :: [Int] -> Int -> Params
+recite ints goal = reciteRecursive $ PS toIntMap (length ints + 1) (last ints) (reverse ints) goal
+    where toIntMap = IM.fromList $ zip ints [[x] | x <- [1..]]
 data Params = PS {
     indexMap :: IM.IntMap [Int],
     count :: Int,
@@ -29,7 +30,10 @@ reciteRecursive params@(PS indexMap' count' lastEntered' ll goal')
     | otherwise = reciteRecursive $ PS newMap (count'+1) numberToInsert (numberToInsert:ll) goal'
     where indexes = IM.findWithDefault [] lastEntered' indexMap'
           numberToInsert = if length indexes >= 2 then let a:b:_ = indexes in a - b else 0
-          valueToInsert = let existing = IM.findWithDefault [] numberToInsert indexMap' in count':existing
+          valueToInsert = let existing = IM.findWithDefault [] numberToInsert indexMap' in case existing of
+                                                                                            (a:b:_) -> [count',a]
+                                                                                            [a] -> [count',a]
+                                                                                            [] -> [count']
           newMap = IM.insert numberToInsert valueToInsert indexMap'
 
 
