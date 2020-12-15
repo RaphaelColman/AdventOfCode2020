34cef60c9b3aae0467291bbf3ce8c2568b58e37c
Raphael.Colman <rcolman@ybs.com>
2020-12-15 13:41:50 +0000

    Simplified

--- src/AoC15/RambunctiousRecitation.hs
+++ src/AoC15/RambunctiousRecitation.hs
@@ -1,41 +1,43 @@
 module AoC15.RambunctiousRecitation where
 
-import qualified Data.IntMap as IM
+import qualified Data.IntMap.Strict as IM
+import Data.Foldable (Foldable(foldl'))
 
 aoc15 :: IO ()
 aoc15 = do
-    print $ recite input 2020
-    print $ recite2020 input 30000000
+  print $ recite input 2020
+  --print $ recite input 30000000
 
 input :: [Int]
-input = [18,8,0,5,4,1,20]
+input = [18, 8, 0, 5, 4, 1, 20]
 
-recite2020 :: [Int] -> Int -> Int
-recite2020 ints goal = lastEntered $ recite ints goal
+recite :: [Int] -> Int -> Int
+recite ints goal = currentNumber $ foldl' reciteFold params ll
+  where
+    params = toParams ints 
+    ll = [len .. (goal-1)]
+    len = length ints
 
-recite :: [Int] -> Int -> Params
-recite ints goal = reciteRecursive $ PS toIntMap (length ints + 1) (last ints) (reverse ints) goal
-    where toIntMap = IM.fromList $ zip ints [[x] | x <- [1..]]
-data Params = PS {
-    indexMap :: IM.IntMap [Int],
-    count :: Int,
-    lastEntered :: Int,
-    ll :: [Int],
-    goal :: Int
-} deriving (Eq, Show)
+data Params = PS
+  { indexMap :: IM.IntMap Int,
+    currentNumber :: Int
+  }
+  deriving (Eq, Show)
 
-reciteRecursive :: Params -> Params
-reciteRecursive params@(PS indexMap' count' lastEntered' ll goal')
-    | count' == goal' + 1 = params
-    | otherwise = reciteRecursive $ PS newMap (count'+1) numberToInsert (numberToInsert:ll) goal'
-    where indexes = IM.findWithDefault [] lastEntered' indexMap'
-          numberToInsert = if length indexes >= 2 then let a:b:_ = indexes in a - b else 0
-          valueToInsert = let existing = IM.findWithDefault [] numberToInsert indexMap' in case existing of
-                                                                                            (a:b:_) -> [count',a]
-                                                                                            [a] -> [count',a]
-                                                                                            [] -> [count']
-          newMap = IM.insert numberToInsert valueToInsert indexMap'
+reciteFold :: Params -> Int -> Params
+reciteFold (PS im currentNumber') count' = newParams
+  where
+    lastIndex = IM.findWithDefault 0 currentNumber' im
+    nextNum = if lastIndex == 0 then 0 else count' - lastIndex
+    newMap = IM.insert currentNumber' count' im
+    newParams = PS newMap nextNum
+
+toParams :: [Int] -> Params
+toParams [] = undefined
+toParams xs = PS (IM.fromList restEnumerated) lst 
+  where
+    lst = last xs
+    restEnumerated = zip (take (length xs -1) xs) [1 ..]
 
 
-toIndexMap :: [Int] -> IM.IntMap [Int]
-toIndexMap ints = IM.fromList $ zip ints [[x] | x <- [1..]]
+test = recite [0,3,6] 3
--- test/AoC15/RambunctiousRecitationSpec.hs
+++ test/AoC15/RambunctiousRecitationSpec.hs
@@ -7,17 +7,17 @@ spec :: Spec
 spec =
     describe "Part 1 test" $ do
         it "can do part 1" $ do
-            recite2020 [1,3,2] 2020 `shouldBe` 1
-            recite2020 [2,1,3] 2020 `shouldBe` 10
-            recite2020 [1,2,3] 2020 `shouldBe` 27
-            recite2020 [2,3,1] 2020 `shouldBe` 78
-            recite2020 [3,2,1] 2020 `shouldBe` 438
-            recite2020 [3,1,2] 2020 `shouldBe` 1836
+            recite [1,3,2] 2020 `shouldBe` 1
+            recite [2,1,3] 2020 `shouldBe` 10
+            recite [1,2,3] 2020 `shouldBe` 27
+            recite [2,3,1] 2020 `shouldBe` 78
+            recite [3,2,1] 2020 `shouldBe` 438
+            recite [3,1,2] 2020 `shouldBe` 1836
 
         xit "can do part 2" $ do
-            recite2020 [1,3,2] 30000000 `shouldBe` 2578
-            recite2020 [2,1,3] 30000000 `shouldBe` 3544142
-            recite2020 [1,2,3] 30000000 `shouldBe` 261214
-            recite2020 [2,3,1] 30000000 `shouldBe` 6895259
-            recite2020 [3,2,1] 30000000 `shouldBe` 18
-            recite2020 [3,1,2] 30000000 `shouldBe` 362
+            recite [1,3,2] 30000000 `shouldBe` 2578
+            recite [2,1,3] 30000000 `shouldBe` 3544142
+            recite [1,2,3] 30000000 `shouldBe` 261214
+            recite [2,3,1] 30000000 `shouldBe` 6895259
+            recite [3,2,1] 30000000 `shouldBe` 18
+            recite [3,1,2] 30000000 `shouldBe` 362
