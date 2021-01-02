2bb766349337fa3cafe643c542d9614f4e30b63d
Raphael <raphael.colman@infinityworks.com>
2021-01-02 14:55:33 +0000

    Day 23 part 2

--- src/AoC23/CrabCups.hs
+++ src/AoC23/CrabCups.hs
@@ -1,11 +1,20 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module AoC23.CrabCups where
 
-import Control.Lens
-import Data.Foldable
+import Control.Lens ()
+import Control.Monad (unless)
+import Control.Monad.Primitive (PrimMonad (PrimState), PrimState)
+import Control.Monad.ST (runST)
+import Control.Monad.Trans.Class (lift)
+import Data.Char (digitToInt, intToDigit)
+import qualified Data.Conduino as C
+import qualified Data.Conduino.Combinators as C
+import Data.Finite ()
+import Data.Foldable (Foldable (foldl'), for_)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
 import Data.IntSet (IntSet)
@@ -13,26 +22,24 @@ import qualified Data.IntSet as IS
 import Data.List.PointedList (moveTo)
 import qualified Data.List.PointedList as PL
 import qualified Data.List.PointedList.Circular as PLC
-import Data.Maybe
+import Data.Maybe (fromJust, fromMaybe)
 import Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
 import Data.Set (Set)
 import qualified Data.Set as S
-import Data.Sort
-import           Data.Finite
-import qualified Data.Vector.Mutable as MV
-import qualified Data.Vector as V
-import GHC.TypeLits
-import Control.Monad.Primitive
-
+import Data.Sort ()
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed.Mutable (MVector)
+import qualified Data.Vector.Unboxed.Mutable as MV
 import Debug.Trace
+import GHC.TypeLits ()
+import Data.Map (Map)
+import qualified Data.Map as M
 
 aoc23 :: IO ()
 aoc23 = do
-  let game = initGame puzzleInput
-  print $ part1 game
-  let part2Game@(MkGame plc lookup) = initGame $ levelUpInput testInput
-  print $ play 5 part2Game
+  --let game = initGame $ levelUpInput puzzleInput
   print "Done"
 
 data Game = MkGame
@@ -42,7 +49,10 @@ data Game = MkGame
   deriving (Eq, Show)
 
 play :: Int -> Game -> Game
-play times game = iterate step' game !! times
+play = go 0 
+  where go count times game
+          | count == times = game
+          | otherwise = go (count+1) times (step' game)
 
 part1 :: Game -> String
 part1 game = concatMap show orderedList
@@ -50,6 +60,11 @@ part1 game = concatMap show orderedList
     (MkGame plc lookup) = play 100 game
     (PLC.PointedList prefix focus suffix) = fromJust $ moveTo (lookup IM.! 1) plc
     orderedList = suffix ++ reverse prefix
+
+part2 :: Game -> [Int]
+part2 MkGame{..} = take 2 suffix
+  where cup1index = lookup IM.! 1
+        plc@(PLC.PointedList prefix focus suffix) = fromJust $ moveTo cup1index pointedList
 
 step' :: Game -> Game
 step' game = progressGame (MkGame movedBackToFocus insertedLookup)
@@ -127,34 +142,50 @@ initGame xs = MkGame plc mp
       let indexZip = zip xs [0 ..]
        in IM.fromList indexZip
 
-testInput :: [Int]
-testInput = [3, 8, 9, 1, 2, 5, 4, 6, 7]
-
-puzzleInput :: [Int]
-puzzleInput = [3, 2, 7, 4, 6, 5, 1, 8, 9]
 
 levelUpInput :: (Ord a, Num a, Enum a) => [a] -> [a]
 levelUpInput input = input ++ rest
-    where maxNum = maximum input
-          rest = [maxNum + 1, maxNum+2 .. 10000000]
+  where
+    maxNum = maximum input
+    rest = [maxNum + 1, maxNum + 2 .. 10000000]
 
-newtype CrabState s = CrabState { csRight  :: MV.MVector s Int }
+----From mstksg
+newtype CrabState s = CrabState {csRight :: MVector s Int}
 
-step :: forall m s. (PrimMonad m, PrimState m ~ s) => CrabState s -> Int -> m Int
-step CrabState{..} lab = do
-    (gs@(g1,_,g3),lab') <- pull3 lab
-    MV.unsafeWrite csRight lab lab'
-    let target = until (notAny gs) subWrap (subWrap lab)
-    aftertarg <- MV.unsafeRead csRight target
-    MV.unsafeWrite csRight target g1
-    MV.unsafeWrite csRight g3 aftertarg
-    pure lab'
+sourceCrabState ::
+  (PrimMonad m, PrimState m ~ s) =>
+  CrabState s ->
+  -- | item to start from
+  Int ->
+  C.Pipe i Int u m ()
+sourceCrabState CrabState {..} i0 = go i0
+  where
+    go i = do
+      j <- lift $ MV.unsafeRead csRight i
+      unless (j == i0) $ do
+        C.yield j
+        go j
+
+step ::
+  forall m s.
+  (PrimMonad m, PrimState m ~ s) =>
+  CrabState s ->
+  Int ->
+  m Int
+step CrabState {..} lab = do
+  (gs@(g1, _, g3), lab') <- pull3 lab
+  MV.unsafeWrite csRight lab lab'
+  let target = until (notAny gs) subWrap (subWrap lab)
+  aftertarg <- MV.unsafeRead csRight target
+  MV.unsafeWrite csRight target g1
+  MV.unsafeWrite csRight g3 aftertarg
+  pure lab'
   where
     n = MV.length csRight
     subWrap x
-      | x == 0    = n - 1
+      | x == 0 = n - 1
       | otherwise = x - 1
-    notAny (g1,g2,g3) x = x /= g1 && x /= g2 && x /= g3
+    notAny (g1, g2, g3) x = x /= g1 && x /= g2 && x /= g3
     {-# INLINE notAny #-}
     pull3 :: Int -> m ((Int, Int, Int), Int)
     pull3 i0 = do
@@ -162,6 +193,93 @@ step CrabState{..} lab = do
       i2 <- MV.unsafeRead csRight i1
       i3 <- MV.unsafeRead csRight i2
       i4 <- MV.unsafeRead csRight i3
-      pure ((i1,i2,i3),i4)
+      pure ((i1, i2, i3), i4)
     {-# INLINE pull3 #-}
 {-# INLINE step #-}
+
+initialize ::
+  forall m s.
+  (PrimMonad m, PrimState m ~ s) =>
+  Vector Int ->
+  -- | initial pointer
+  m (Int, CrabState s)
+initialize v0 = do
+  csRight <- MV.unsafeNew n
+  for_ [0 .. n -1] $ \i ->
+    MV.unsafeWrite csRight (v0 V.! subWrap i) (v0 V.! i)
+  let i0 = v0 V.! 0
+  pure (i0, CrabState {..})
+  where
+    n = V.length v0
+    subWrap x
+      | x == 0 = n - 1
+      | otherwise = x - 1
+{-# INLINE initialize #-}
+
+run ::
+  (PrimMonad m, PrimState m ~ s) =>
+  Int ->
+  Int ->
+  CrabState s ->
+  m ()
+run n i0 cs = go 0 i0
+  where
+    go !m !i
+      | m == n = pure ()
+      | otherwise = go (m + 1) =<< step cs i
+{-# INLINE run #-}
+
+sParse :: String -> Maybe (Vector Int)
+sParse = Just . V.fromListN 1000000 . (++ [9 ..]) . map toIx
+
+sSolve :: Vector Int -> Maybe [Int]
+sSolve v0 = Just $
+  runST $ do
+    (i0, cs) <- initialize v0
+    run 10000000 i0 cs
+    C.runPipe $
+      sourceCrabState cs 0
+        C..| C.map fromIx
+        C..| C.take 2
+        C..| C.sinkList
+
+toIx :: Char -> Int
+toIx = subtract 1 . digitToInt
+
+fromIx :: Int -> Int
+fromIx = (+ 1)
+
+
+
+data Cup = MkCup {
+  lowerCup :: Cup,
+  nextCup :: Cup,
+  value :: Int
+} deriving (Eq)
+
+instance Show Cup where
+  show (MkCup lwr nxt val) = "Cup: " ++ show val ++ " lwr: " ++ show lwrVal ++ " next: " ++ show (value nxt)
+          where lwrVal = value lwr
+
+
+makeCups :: [Int] -> IntMap Cup
+makeCups xs@(start:rest) = res
+  where res = IM.fromSet mkcup $ IS.fromList xs
+        nexts = IM.fromList $ zip xs rest
+        --max' = maximum xs --Might need to hardcode this
+        max' = 10000000
+        mkcup val 
+            | val <= 1 = MkCup (res IM.! max') nextCup val
+            | otherwise = MkCup (res IM.! (val-1)) nextCup val
+            where nextCupLabel = IM.findWithDefault start val nexts
+                  nextCup = res IM.! nextCupLabel
+
+
+describeCup :: IS.Key -> Cup
+describeCup i = makeCups (levelUpInput testInput) IM.! i
+
+testInput :: [Int]
+testInput = [3, 8, 9, 1, 2, 5, 4, 6, 7]
+
+puzzleInput :: [Int]
+puzzleInput = [3, 2, 7, 4, 6, 5, 1, 8, 9]
