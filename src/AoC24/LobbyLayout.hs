b50048a2d431ccdd6b1f8c475d8fbd0e8c6d402d
Raphael Colman <raphael.colman@infinityworks.com>
2021-01-03 20:32:23 +0000

    Day 24 part 1

--- src/AoC24/LobbyLayout.hs
+++ src/AoC24/LobbyLayout.hs
@@ -1,18 +1,43 @@
 module AoC24.LobbyLayout where
 
-import Common.Utils
-import Text.Trifecta
-import Control.Applicative
+import           Common.Utils
+import           Control.Applicative
+import           Data.Foldable
+import           Data.Map            (Map)
+import qualified Data.Map            as M
+import           Linear.V2
+import           Text.Trifecta
 
 aoc24 :: IO ()
 aoc24 = do
     contents <- getInputFile 24
     let directionLists = parseString parseInput mempty contents
-    print directionLists
+    print $ part1 <$> directionLists
     print "done"
 
 data Direction = East | SouthEast | SouthWest | West | NorthWest | NorthEast deriving (Enum, Eq, Show, Ord, Bounded)
 type DirectionList = [Direction]
+type Coord = V2 Int
+
+part1 :: [DirectionList] -> Int
+part1 dls = length $ M.filter odd freqMap
+    where freqMap = toTileFlips dls
+
+toTileFlips :: [DirectionList] -> Map Coord Int
+toTileFlips = freqs . map getToTile
+
+getToTile :: DirectionList -> V2 Int
+getToTile = foldl' (\a v -> a + directionToVector v) (V2 0 0)
+
+--Doing this with the origin in the bottom left
+directionToVector :: Direction -> V2 Int
+directionToVector direction = case direction of
+                                East      -> V2 2 0
+                                SouthEast -> V2 1 (-1)
+                                SouthWest -> V2 (-1) (-1)
+                                West      -> V2 (-2) 0
+                                NorthWest -> V2 (-1) 1
+                                NorthEast -> V2 1 1
 
 --Parsing
 parseInput :: Parser [DirectionList]
@@ -23,7 +48,7 @@ parseLine = some parseDirection
 
 parseDirection :: Parser Direction
 parseDirection = try parseEastWest <|> try parseComposite
-   
+
 parseEastWest :: Parser Direction
 parseEastWest = do
     eastWest <- oneOf "ew"
@@ -40,4 +65,8 @@ parseComposite = do
         "sw" -> return SouthWest
         "ne" -> return NorthEast
         "nw" -> return NorthWest
-        _ -> fail "Invalid composite direction"
+        _    -> fail "Invalid composite direction"
+
+--Examples
+itself = "nwwswee"
+southEast = "esew"
