2dcf0a559784273d2de7aedc5712688b354eb6d1
Raphael <raphael.colman@infinityworks.com>
2021-02-25 14:49:55 +0000

    Haversack parsing

--- src/AoC7/Haversacks.hs
+++ src/AoC7/Haversacks.hs
@@ -1,16 +1,22 @@
 module AoC7.Haversacks where
 
 import           Common.Utils
-import           Data.List.Split
+import           Data.List.Split hiding (sepBy)
 import qualified Data.Map        as M
 import qualified Data.Set        as S
 import Text.Trifecta
+import Control.Applicative ((<|>))
 
 aoc7 :: IO ()
 aoc7 = do
   contents <- getInputFile 7
-  let p str = parseString parseBag mempty str
-  print $ p "faded blue bags"
+  let p parser str = parseString parser mempty str
+  --print $ p parseBag "wavy tan bags."
+  --print $ p parseNumBags "2 dull crimson bags"
+  --print $ p parseBagMap "2 mirrored green bags, 2 dull crimson bags, 2 drab tan bags, 1 vibrant coral bag."
+  print $ p parseBagRule "dotted plum bags contain 2 mirrored green bags, 2 dull crimson bags, 2 drab tan bags, 1 vibrant coral bag."
+  print $ p parseBagRule "drab tomato bags contain no other bags."
+  print $ p parseBagRules contents
 
 type BagRules = M.Map Bag (M.Map Bag Int)
 
@@ -29,10 +35,34 @@ parseBag = do
   colour <- some letter
   whiteSpace 
   string "bag"
+  skipOptional $ char 's'
+  skipOptional $ char '.'
   pure $ BG hue colour
 
-parseBagRule :: Parser BagRules
-parseBagRule = undefined
+parseNumBags :: Parser (Bag, Int)
+parseNumBags = do
+  num <- integer
+  bag <- parseBag
+  pure (bag, fromIntegral num)
+
+parseBagMap :: Parser (M.Map Bag Int)
+parseBagMap = do
+  let noOtherBags = fmap (const []) $ try $ string "no other bags"
+  let bags = sepBy parseNumBags $ string ", " 
+  bagsResult <- noOtherBags <|> bags
+  pure $ M.fromList bagsResult
+
+parseBagRule :: Parser (Bag, M.Map Bag Int)
+parseBagRule = do
+  containingBag <- parseBag
+  whiteSpace >> string "contain" >> whiteSpace
+  bagMap <- parseBagMap
+  pure (containingBag, bagMap)
+
+parseBagRules :: Parser BagRules
+parseBagRules = do
+  bagRules <- some (token parseBagRule)
+  pure $ M.fromList bagRules
 
 parseInput :: String -> BagRules
 parseInput = M.fromList . map parseLine . lines
